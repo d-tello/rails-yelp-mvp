6b1c5938e12d8f469ebc89f439bf997c036e8a0a
Davide Tello <tello.davide@gmail.com>
2021-02-17 12:38:07 +0100

    Add Models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      # get 'top', to: 'restaurants#top'
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+
+    resources :reviews, only: %i[new create]
+  end
+
+  resources :reviews, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seed for the creation of fake data
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+category = %w[chinese italian japanese french belgian]
+5.times do
+  restaurant = Restaurant.create!(name: Faker::Restaurant.name,
+                                  address: Faker::Address.street_address,
+                                  category: category.sample)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
